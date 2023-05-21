d8f73cf6db68da3aee93ab32a70a6bba387f4c81
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-05-21 12:13:59 +0530

    Added Redux store

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
-import Body from "./components/Body"; 
+import Body from "./components/Body";
 import Footer from "./components/Footer";
-// import About from './components/About.jsx';
 import Shimmer from "./components/Shimmer.jsx";
-import Contact from './components/Contact.jsx';
-import Cart from './components/Cart.jsx'
+import Contact from "./components/Contact.jsx";
+import Cart from "./components/Cart.jsx";
 import Resturant from "./components/Resturant.jsx";
 import Error from "./components/Error.jsx";
 import Profile from "./components/ProfileClass.jsx";
-import {createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 /*    
     Header
         -Logo
@@ -29,62 +30,65 @@ import {createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
         -Copyright
 */
 
-const Instamart = lazy(() => import('./components/Instamart.jsx'));
-const About = lazy(() => import('./components/About.jsx'));
+const Instamart = lazy(() => import("./components/Instamart.jsx"));
+const About = lazy(() => import("./components/About.jsx"));
 
 const Applayout = () => (
-    <>
-        <Header/>
-        <Outlet/>
-        <Footer/>
-    </>
+  <Provider store={store}>
+    <Header />
+    <Outlet />
+    <Footer />
+  </Provider>
 );
 
-
 const appRouter = createBrowserRouter([
-    {
-        path: '/',
-        element: <Applayout />,
-        errorElement: <Error/>,
+  {
+    path: "/",
+    element: <Applayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
         children: [
-            {
-                path: '/',
-                element: <Body />,
-            },
-            {
-                path: '/about',
-                element: <Suspense fallback={<h1>Loading...</h1>}>
-                <About />
-                </Suspense>,
-                children: [{
-                    path: 'profile',
-                    element: <Profile />,
-                }]
-            },
-            {
-                path: '/contact',
-                element: <Contact />,
-            },
-            {
-                path: '/cart',
-                element: <Cart />,
-            },
-            {
-                path: '/resturants/:id',
-                element: <Resturant/>,
-            },
-            {
-                path: '/instamart',
-                element:  <Suspense fallback = {<Shimmer/>}>
-                <Instamart />
-                </Suspense>,
-            }
-
+          {
+            path: "profile",
+            element: <Profile />,
+          },
         ],
-    },
-    
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/resturants/:id",
+        element: <Resturant />,
+      },
+      {
+        path: "/instamart",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Instamart />
+          </Suspense>
+        ),
+      },
+    ],
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);
